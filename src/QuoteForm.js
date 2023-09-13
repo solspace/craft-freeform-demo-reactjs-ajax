d37b187ef3d072a953864dd030589e48b7ff6f5d
Sean Delaney <sean@solspace.com>
2023-09-13 16:52:11 +0100

    Prevent captcha error key throwing querySelector errors

--- src/QuoteForm.js
+++ src/QuoteForm.js
@@ -181,11 +181,13 @@ const Form = () => {
                 showSubmissionError();
 
                 for (const [key, value] of Object.entries(response.errors)) {
-                    const element = document.querySelector(`.${key}-field .error-message`);
-                    if (element) {
-                        element.innerHTML = value[0];
-                        element.classList.add('flex');
-                        element.classList.remove('hidden');
+                    if (!/^-?\d+$/.test(key)) {
+                        const element = document.querySelector(`.${key}-field .error-message`);
+                        if (element) {
+                            element.innerHTML = value[0];
+                            element.classList.add('flex');
+                            element.classList.remove('hidden');
+                        }
                     }
                 }
             }
